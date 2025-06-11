818f3a7770f1cac4c2f6de02bce17649938fbd5a
Umaya <umayadharsh@gmail.com>
2025-06-11 14:30:13 +0530

    Switch to using 'serve' for production build

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 app.use(cors());
 app.use(express.json());
